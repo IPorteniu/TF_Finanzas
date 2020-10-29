180d9eb9491ab58a67d2edf36c159eb0cb199ac3
IPorteniu <43084237+IPorteniu@users.noreply.github.com>
2020-10-29 11:34:40 -0500

    Customer tables updates

--- src/router.js
+++ src/router.js
@@ -8,6 +8,9 @@ export default new Router({
     routes: [
         { path: "/", alias: "/subscribers", name: "subscribers", component: () => import("./components/SubscribersList") },
         { path: "/subscribers/:id", name: "subscriber-details", component: () => import("./components/Subscriber") },
+        { path: "/", alias: "/customers", name: "customers", component: () => import("./components/Customers/CustomersList") },
+        { path: "/customers/:id", name: "customer-details", component: () => import("./components/Customers/Customer") },
+        { path: "/register1", name: "register", component: () => import("./components/Customers/AddCustomer")},
         { path: "/register", name: "register", component: () => import("./components/AddSubscriber")}
-    ]
+    ] 
 });
